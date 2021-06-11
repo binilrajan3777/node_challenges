f1472bf9d31995738f44fc5e533af432c68e5c64
binilrajan3777 <85624593+binilrajan3777@users.noreply.github.com>
2021-06-11 19:09:25 +0530

    Node Challenge

--- config/appconfig.js
+++ config/appconfig.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 
 module.exports = {
 	app: {
-		port: process.env.DEV_APP_PORT || 1358,
-		appName: process.env.APP_NAME || 'Test',
+		port: process.env.PORT || 1358,
+		appName: process.env.APP_NAME || 'Node Challenge',
 	},
 	auth: {
-		jwt_secret: process.env.JWT_SECRET,
+		jwt_secret: process.env.JWT_SECRET||'VmVyeVBvd2VyZnVsbFNlY3JldA==',
 		jwt_expiresin: process.env.JWT_EXPIRES_IN || '1d',
 		//saltRounds: process.env.SALT_ROUND || 10,
 		//refresh_token_secret: process.env.REFRESH_TOKEN_SECRET || 'VmVyeVBvd2VyZnVsbFNlY3JldA==',
